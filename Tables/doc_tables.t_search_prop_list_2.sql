ea5df2458082eb6d7bec7cd044369d3c54895ff9
test < test@devart>
2017-07-13 17:28:31 +0300

    Resolve #1

---
+++ Tables/doc_tables.t_search_prop_list_2.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [doc_tables].[t_search_prop_list_2] (
+  [ID] [int] NOT NULL,
+  [name] [int] NOT NULL,
+  [c_image ] [image] NULL,
+  [c_varb] [varbinary](max) NULL
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE CLUSTERED INDEX [idx_t_search_prop_list_id]
+  ON [doc_tables].[t_search_prop_list_2] ([ID])
+  ON [PRIMARY]
+GO
+
+CREATE UNIQUE INDEX [ui_t_search_prop_list]
+  ON [doc_tables].[t_search_prop_list_2] ([ID])
+  ON [PRIMARY]
+GO
+
+CREATE UNIQUE INDEX [ui_t_search_prop_list_2]
+  ON [doc_tables].[t_search_prop_list_2] ([name])
+  ON [PRIMARY]
+GO
