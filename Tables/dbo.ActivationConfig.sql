80ddde3688f1b4da576cde9685e22d3de550487e
test < test@devart>
2017-07-13 16:31:57 +0300

    Fixes #1

---
+++ Tables/dbo.ActivationConfig.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ActivationConfig] (
+  [Name] [varchar](50) NULL,
+  [SiteCode] [varchar](50) NULL,
+  [V2ersion] [varchar](50) NULL,
+  [column1] [varchar](50) NULL
+)
+ON [PRIMARY]
+GO
