f69be9b75e20bde18b3fe843dae06f496c9d6b2e
test < test@devart>
2017-07-13 17:08:03 +0300

    Open #1

---
+++ Tables/tax.big_table1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [tax].[big_table1] (
+  [id] [int] NOT NULL,
+  [name] [nvarchar](50) NULL,
+  [second name] [nvarchar](50) NULL,
+  [email] [nvarchar](50) NULL,
+  CONSTRAINT [PK_big_table1_id] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
