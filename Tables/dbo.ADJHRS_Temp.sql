1764558f8d831b224e809dacd427788d0264dd24
test < test@devart>
2017-07-13 16:33:14 +0300

    Closed #1

---
+++ Tables/dbo.ADJHRS_Temp.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[ADJHRS_Temp] (
+  [STORE] [int] NULL,
+  [EMPL_NUM] [int] NULL,
+  [ADJ_TYPE] [int] NULL,
+  [N_DATE] [date] NULL,
+  [N_TIME_IN] [varchar](50) NULL,
+  [N_TIME_OUT] [varchar](50) NULL,
+  [N_JOB_NUM] [int] NULL,
+  [O_DATE] [date] NULL,
+  [O_TIME_IN] [varchar](50) NULL,
+  [O_TIME_OUT] [varchar](50) NULL,
+  [O_JOB_NUM] [int] NULL,
+  [A_DATE] [date] NULL,
+  [A_TIME] [varchar](50) NULL,
+  [A_MNGR_NUM] [int] NULL,
+  [REASON_NUM] [int] NULL,
+  [REASON_TXT] [varchar](50) NULL
+)
+ON [PRIMARY]
+GO
